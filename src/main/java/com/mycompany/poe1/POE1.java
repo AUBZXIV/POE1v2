f9abe0ee2d22fbfdc541baf4086fdf4107a3ee57
AUBZXIV <ST10518249@rcconnect.edu.za>
2026-09-15 17:02:22 +0200

    more variables and while loops

--- src/main/java/com/mycompany/poe1/POE1.java
+++ src/main/java/com/mycompany/poe1/POE1.java
@@ -14,71 +14,51 @@ public class POE1 {
    Scanner scanner = new Scanner( System.in);
    // 
    String username="";
-  
-  
    String under="_";
-        System.out.println("Welcome to the APP!!");
-        System.out.println("Please create your account below");
+   int ulen=username.length();
+        System.out.println("WELCOME TO WAVE!");
+        System.out.println("Please create your account below(Press OK to continue)");
+        String answer=scanner.nextLine();
         
-       
-        int count=0;
-     /*   
-        System.out.println("PLEASE CREATE YOUR USERNAME :");
+        System.out.println("Please create your username");
         username=scanner.nextLine();
-        int ulen=username.length();
-        
-       while(ulen < 5 || !username.contains(under)){// either of the conditions must be false not both, hence"||"
-       //count++;
-         System.out.println("Username is INVALID.");
-    System.out.println("Username must be at least 5 characters and contain '_'.");
-        
-    System.out.println("PLEASE CREATE YOUR USERNAME:");
-    username = scanner.nextLine();
-    
-    ulen = username.length();
-           
-       }
-      System.out.println("Username is valid!");
-      
-     //---- cellphone validation
-      String cellphone;
-        System.out.println("PLEASE ENTER YOUR CELLPHONE NUMBER");
+        while(username.isBlank()){
+            System.out.println("Please fill in your username*");
+            username=scanner.nextLine();
+        }
+        while (ulen>5||!username.contains(under)) {            
+            System.out.println("Username is invalid");
+            System.out.println("Username should contain an underscore and must be 5 characters long");
+            System.out.println("Please re enter user name");
+            username=scanner.nextLine();
+        }
+         System.out.println("Username is valid!");   
+         
+         String codesa="+27";
+         String cellphone=scanner.nextLine();
+         int lcell=cellphone.length();
+         
+        System.out.println("Please enter your cellphone number");
         cellphone=scanner.nextLine();
         
-       String codesa="+27";
-       int clen=cellphone.length();
-       String number="728638153";
-       
-       String con=codesa.concat(number);
-       int pplen=con.length();
-        System.out.println(pplen);
-        
-        
-       
-        while (clen>12||!cellphone.startsWith(codesa)) {            
-            System.out.println("Cellphone format is invalid");
-            System.out.println("Number should start with country code");
-            System.out.println("Cellphone shouldnt be longer than 10 digits");
-            
-            System.out.println("Please REenter your cellphone number");
+        while (cellphone.isBlank()) {            
+            System.out.println("Please fill in your cellphone number*");  
             cellphone=scanner.nextLine();
-             
         }
-        System.out.println("CELLPHONE NUMBER IS VALID!!");
         
-        //--- PASSWORD VALIDATION
-        */
-  
-  /*
-     System.out.println("Please enter your password:");
-String password = scanner.nextLine();
-
-int plen = password.length();
-
-System.out.println("Password length: " + plen);
-    
-  */
-  
+        
+        while (lcell>12||!cellphone.startsWith(codesa)) {            
+            System.out.println("Cellphone is INVALID!");
+            System.out.println("Cellphone should not be longer than 10 characters and should start with your country code");
+             System.out.println("Please re enter your cellphone number");    
+            cellphone=scanner.nextLine();
+        }
+         System.out.println("Cellphone number is valid!");       
+        
+        
+ 
+       
+            
   String password=scanner.nextLine();
   int whilecount=0;
         System.out.println("PLEASE CREATE YOUR PASSWORD BELOW");
@@ -98,10 +78,10 @@ System.out.println("Password length: " + plen);
        password=scanner.nextLine();
       
         plen = password.length(); 
-       String wwe="";
+       
    }     
         System.out.println("Password is valid"); ////
-        Character ch;
+       /* Character ch;
      
         for (int i = 0; i <plen; i++) {
         ch=password.charAt(i);
@@ -112,7 +92,7 @@ System.out.println("Password length: " + plen);
         }
         
    
-        
+        */
         
         
         
